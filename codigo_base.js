4b4c675b54d81cf0c2a005f1ece4f8afc23f143a
Jordy Araujo <jordyfaria0@gmail.com>
2018-03-26 16:23:40 -0300

    Implementado funções /HELP e  /NICK.

--- codigo_base.js
+++ codigo_base.js
@@ -7,6 +7,8 @@ var clients = [];
 // Start a TCP Server
 net.createServer(function (socket) {
 
+  var nickname = "Annonymous";
+
   // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort
 
@@ -14,8 +16,7 @@ net.createServer(function (socket) {
   clients.push(socket);
 
   // Send a nice welcome message and announce
-  socket.write("Welcome " + socket.name + "\n");
-  broadcast(socket.name + " joined the chat\n", socket);
+  socket.write("Welcome!\n/HELP to know all commands.\n");
 
   // Handle incoming messages from clients.
   socket.on('data', function (data) {
@@ -34,34 +35,34 @@ net.createServer(function (socket) {
     clients.forEach(function (client) {
       // Don't want to send it to sender
       if (client === sender) return;
-      client.write(message);
+      client.write(nickname+": "+message);
     });
     // Log it to the server output too
     process.stdout.write(message)
   }
 
+  // Analize if is a command or a message
   function analize(data){
-    var mensagem = String(data).trim();
-    var args = mensagem.split(" ");
-    if ( args[0] == "NICK" ) nick(args);  
-    else if ( args[0] == "USER") user(args);
-    else if ( args[0] == "JOIN") join(args);
-    else socket.write("ERRO: Non-existent command");
+    var message = String(data).trim();
+    var args = message.split(" ");
+    if ( args[0] == "/NICK") nick(args);
+    else if (args[0] == "/HELP") help();
+    //else if ( args[0] == "/USER") user(args); TODO: Pra que serviria ?
+    //else if ( args[0] == "/JOIN") join(args); TODO: Pra que serviria ?
+    else broadcast(data);
   }
 
-  function user(args){
-    socket.write("USER command executed with sucess.\n");
-  }
-
-
+  // Set the user's nickname
   function nick(args){
+    nickname = args[1];
+    broadcast(nickname + " joined the chat\n", socket);
     socket.write("NICK command executed with sucess.\n");
   }
 
-  function join(args){
-    socket.write("JOIN command executed with sucess.\n");
+  // List all commands available
+  function help(){
+    socket.write("Commands:\n/NICK: To set your nickname.\n");
   }
-
 
 }).listen(5000);
 
