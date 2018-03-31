d42f3ce88a3a2f8008557c24b054abd47b49673e
Rafael Ratacheski de Sousa Raulino <rafaelratacheski@gmail.com>
2018-03-31 15:17:53 -0300

    Criada a entidade Cliente

---
+++ entity/entity_client.js
@@ -0,0 +1,17 @@
+ exports.constructor = function(socket){
+ 	var objClient = new Object();
+ 	objClient.socket = socket;
+ 	objClient.away = false;
+ 	objClient.nick = null;
+ 	objClient.awayMessage = "";
+ 	objClient.channels = [];
+ 	objClient.isOp = false;
+ 	objClient.modes = [];
+	objClient.visible = true;
+ 	objClient.pswdtmp=null;
+ 	objClient.pswd=null;
+	objClient.quitMessage = null;
+	objClient.isRegistered = false;
+
+ 	return objClient;
+}
