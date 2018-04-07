0d6312f8bf53bf477431ef38bdd95e16093c345a
Tiago Santana de Castro <tiagosantanadecastro@gmail.com>
2018-04-07 13:21:02 -0300

    Implementação das funções join, quit e função auxiliar remove

--- handler/handler.js
+++ handler/handler.js
@@ -52,8 +52,8 @@ exports.analyze = function (data, client, clients) {
   else if (args[0] === COMMANDS.OPER) oper(args, client.socket);
   else if (args[0] === COMMANDS.MODE) mode(args, client.socket);
   else if (args[0] === COMMANDS.SERVICE) service(args, client.socket);
-  else if (args[0] === COMMANDS.QUIT) quit(args, client.socket);
-  else if (args[0] === COMMANDS.JOIN) join(args, client.socket);
+  else if (args[0] === COMMANDS.QUIT) quit(args, client, clients);
+  else if (args[0] === COMMANDS.JOIN) join(args, client, clients, nicks, channels);
   else if (args[0] === COMMANDS.PART) part(args, client.socket);
   else if (args[0] === COMMANDS.TOPIC) topic(args, client.socket);
   else if (args[0] === COMMANDS.NAMES) names(args, client.socket);
@@ -152,14 +152,51 @@ function pass(args, client) {
 
 // Client quit from the server
 function quit(args, client, clients) {
+	 var socket = client.socket;
+  if(!args[1]){
+    // Remove usuario sem mostrar nenhuma mensagem
+    if(handler.broadcast(client.nick + " quits\n", client));
+    socket.end();
+    remove(client);
+  } else {
+    // Remove usuario exibindo mensagem escrita por ele
+    args.splice(0, 1);
+    var mesg = args.join(" ");
+    handler.broadcast(client.nick + " quits: " + mesg + "\n", client, clients);
+    socket.end();
+    remove(client);
+  }
 }
 
 // Remove the client from the server
 function remove(client) {
+	 delete server.nicks[client.nick];
+    var index = server.clients.indexOf(client);
+    server.clients.splice(index, 1);
 }
 
 // Client enter in a channel
 function join(args, client, clients, nicks, channels) {
+	var socket = client.socket;
+
+    if (!args[1]) {
+        socket.write("ERROR: invalid request, try /join <#channel>\n");
+        return;
+    }
+    else {
+        var channelName = args.slice(1);
+
+        for (i = 0; i < channels.length; i++) {
+            //checa se o canal existe
+            if (channels[i].name == channelName) {
+                var chn = channels[i];
+                client.channels.push(chn);
+                channels[i].members.push(client);
+                server.channels = channels;
+                client.socket.write("You joined " + chn.name + ".\n");
+            }
+        }
+    }
 }
 
 function privmsg(args, client, clients, channels) {
