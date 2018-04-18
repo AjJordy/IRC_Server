206497c861de8ed900d231daca31af9ca90ef77b
Victor G Ferraz <victorgomferraz2@gmail.com>
2018-04-17 23:25:01 -0300

    Adicionando senha ao JOIN e fix privemsg.

--- spec/handler/cmd-join.spec.js
+++ spec/handler/cmd-join.spec.js
@@ -11,18 +11,28 @@ describe("Deve adicionar cliente ao canal", function () {
         var clients = clientFactory.createClientList();
 
         var channel = channelFactory.createChannelWithClients();
+        var channels = [channel];
         var clientAtivo = clients[0];
         var allClientsWithoutAtivo = [].concat(clients).splice(1);
         var allClients = [].concat(clients).concat(channel.members);
 
+        var password = 'pass123';
         var channelName = channel.name;
+        channel.key = password;
 
-        var comando = 'JOIN ' + channelName;
-        handler.analyze(comando, clientAtivo, allClients, [channel]);
+        var newchannel = '#newChannel';
+        var passNewchannel = 'pass';
+
+        var comando = 'JOIN ' + newchannel + ',' + channelName + ' ' + passNewchannel + ',' + password;
+        handler.analyze(comando, clientAtivo, allClients, channels);
 
 
-        var esperadoAtivo = "You joined " + channelName + ".\n";
-        expect(esperadoAtivo).toBe(clientAtivo.socket.getRespostas()[0]);
+        var esperadoAtivo1 = "You joined " + newchannel + ".\n";
+        var esperadoAtivo2 = "You joined " + channelName + ".\n";
+        expect(esperadoAtivo1).toBe(clientAtivo.socket.getRespostas()[0]);
+        expect(esperadoAtivo2).toBe(clientAtivo.socket.getRespostas()[1]);
+
+        expect(passNewchannel).toBe(channels[1].key);
 
         var esperadoDentroCanal = clientAtivo.nick + ': ' + clientAtivo.nick + " joined the chat\n";
         channel.members.forEach(function (client) {
